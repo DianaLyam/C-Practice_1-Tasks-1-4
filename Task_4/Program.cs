709d286a0ee5c6135a42fc26e3a47edb2de5de4a
Diana Lyamina <lyaminadv@list.ru>
2022-06-09 00:32:44 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число:");
+string str1 = Console.ReadLine();
+int N = int.Parse(str1);
+int i = 1;
+if (N > 0)
+{
+    while (i <= N)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+        i++;
+
+    }
+}
+else
+{
+    while (i >= N)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+        i = i-1;
+
+    }
+}
