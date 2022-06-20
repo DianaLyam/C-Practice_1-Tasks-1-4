3e64671102d8f70df9494e70b8962d1596e3a9a8
Diana Lyamina <lyaminadv@list.ru>
2022-06-20 14:00:32 +0300

    Решение второй задачи, семинар 4

---
+++ Seminar_4_Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int promptNumber (string message)
+{
+    Console.Write (message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int SumDigits (int number)
+{
+    int digits = 0;
+    while (number > 0) 
+    {
+        digits=digits+number%10; 
+        number = number / 10; 
+    }
+    return digits;
+}
+
+int number = promptNumber("Введите число: ");
+Console.WriteLine($"Сумма цифр числа {number} равна {SumDigits(number)}");
