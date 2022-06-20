dd1813f03521196675a6557ba1e9edbf3d8895e5
Diana Lyamina <lyaminadv@list.ru>
2022-06-20 13:26:57 +0300

    решение третьей задачи, семинар 3

---
+++ Seminar_3_Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+int prompt(string msg)
+{
+    System.Console.WriteLine(msg);
+    string N = System.Console.ReadLine(); 
+    return int.Parse(N);
+}
+int powerx(int based, int power)
+{
+    int result=1;
+    for (int i =0; i<power;i++)
+    {
+        result=result*based;
+    }
+    return result;
+}
+int N=prompt("Введите число: ");
+for (int i =1; i<N;i++)
+{
+    Console.Write($"{powerx(i,3)}, ");
+}
+Console.WriteLine (powerx(N,3)+".");
